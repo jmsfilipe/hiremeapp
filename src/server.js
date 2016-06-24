a8d34d8934b8feec96928326eb0ce973fc3e5388
carantunes <carinadeoliveiraantunes@gmail.com>
2016-06-24 22:46:05 +0100

    [ADD] initial node-sass compiler setup

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
 // server.js
 
 // set up ========================
-var express  = require('express');
-var app      = express();                               // create our app w/ express
-var morgan = require('morgan');             // log requests to the console (express4)
-var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
-var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+var express  = require('express'),
+    app      = express(),                             // create our app w/ express
+    morgan = require('morgan'),             // log requests to the console (express4)
+    bodyParser = require('body-parser'),    // pull information from HTML POST (express4)
+    methodOverride = require('method-override'), // simulate DELETE and PUT (express4)
+    stylesheetSrcPath = __dirname + '/app/assets/sass',
+    stylesheetDestPath = __dirname + '/app/public/styles';
 
 // configuration =================
-
-app.use(express.static(__dirname + '/app'));                    // set the static files location /public/img will be /img for users
 app.use('/scripts', express.static(__dirname + '/node_modules'));
 app.use('/src', express.static(__dirname + '/src'));
 app.use(morgan('dev'));                                         // log every request to the console
@@ -17,6 +17,18 @@ app.use(bodyParser.urlencoded({'extended':'true'}));            // parse applica
 app.use(bodyParser.json());                                     // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
+
+//sass configuration
+var sassMiddleware = require('node-sass-middleware');
+app.use('/styles', sassMiddleware({
+  src: stylesheetSrcPath,
+  dest: stylesheetDestPath,
+  debug: true,
+  outputStyle: 'expanded'
+}));
+
+// set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/app'));                    
 
 // listen (start app with node server.js) ======================================
 app.listen(8080);
