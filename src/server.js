9b0cba930c7e4bb3789e338a5aad8e9526fa3acf
carantunes <carinadeoliveiraantunes@gmail.com>
2016-06-25 11:18:13 +0100

    [ADD] serve static config

--- src/server.js
+++ src/server.js
@@ -6,12 +6,12 @@ var express  = require('express'),
     morgan = require('morgan'),             // log requests to the console (express4)
     bodyParser = require('body-parser'),    // pull information from HTML POST (express4)
     methodOverride = require('method-override'), // simulate DELETE and PUT (express4)
-    stylesheetSrcPath = __dirname + '/app/assets/sass',
-    stylesheetDestPath = __dirname + '/app/public/styles';
+    sassMiddleware = require('node-sass-middleware'),
+    stylesheetSrcPath = __dirname +'/app/assets/stylesheets';
+
 
 // configuration =================
-app.use('/scripts', express.static(__dirname + '/node_modules'));
-app.use('/src', express.static(__dirname + '/src'));
+
 app.use(morgan('dev'));                                         // log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());                                     // parse application/json
@@ -19,16 +19,15 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse applica
 app.use(methodOverride());
 
 //sass configuration
-var sassMiddleware = require('node-sass-middleware');
-app.use('/styles', sassMiddleware({
-  src: stylesheetSrcPath,
-  dest: stylesheetDestPath,
-  debug: true,
-  outputStyle: 'expanded'
+app.use('/assets/stylesheets', sassMiddleware({
+    src: stylesheetSrcPath,
+    debug: true,
+    outputStyle: 'expanded'
 }));
 
-// set the static files location /public/img will be /img for users
-app.use(express.static(__dirname + '/app'));                    
+// set the static files location /app/img will be /img for users
+app.use('/scripts', express.static(__dirname + '/node_modules'));
+app.use(express.static(__dirname + '/app'));
 
 // listen (start app with node server.js) ======================================
 app.listen(8080);
@@ -40,6 +39,6 @@ console.log("App listening on port 8080");
 // get all todos
 app.get('/api/todos', function(req, res) {
 
-  res.json("cenas"); // return all todos in JSON format
+    res.json("cenas"); // return all todos in JSON format
 
 });
