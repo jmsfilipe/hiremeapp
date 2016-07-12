2b5e00f3f48009b8d4420b49dc7ffcc89405edc4
Jorge Saldanha <jmsfilipe@gmail.com>
2016-07-12 10:35:06 +0100

    [ADD] questions wip

--- src/app/src/game/QuestionController.js
+++ src/app/src/game/QuestionController.js
@@ -2,7 +2,7 @@
 var question = angular.module('hiremeapp.question', [
     'ngMaterial'
 ])
-.controller('QuestionController', function($timeout, $log, $scope, $state, $stateParams, questionServices){
+.controller('QuestionController', function($timeout, $log, $scope, $state, $stateParams, $mdDialog, questionServices){
     var self = this;
     self.user = $stateParams.user;
     self.filters = [];
@@ -21,5 +21,41 @@ var question = angular.module('hiremeapp.question', [
       });
     };
 
+    self.evaluateAnswer = function(answer, $event){
+      if(answer.correct){
+        $($event.currentTarget).css("background-color", "green");
+        self.showSuccessDialog();
+      } else{
+        $($event.currentTarget).css("background-color", "red");
+      }
+    }
+
+    self.showSuccessDialog = function(ev) {
+
+        $mdDialog.show({
+            controller: 'SuccessDialogController as dialog',
+            templateUrl: "app/src/game/successDialog.html",
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose:true
+        })
+            .then(function(answer) {
+              console.log("ANOTHER")
+            self.showQuestion(self.filters);
+        }, function() {
+            //canceled
+        });
+    };
+
     self.showQuestion(self.filters);
+})
+.controller('SuccessDialogController', function($scope, $mdDialog){
+    var self = this;
+
+    self.cancel = function() {
+        $mdDialog.cancel();
+    };
+    self.save = function() {
+        $mdDialog.hide();
+    };
 });
