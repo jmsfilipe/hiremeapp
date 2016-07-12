627af0f9aff2f2293f89e7ac8b8f0929de72a043
Jorge Saldanha <jmsfilipe@gmail.com>
2016-07-12 14:10:49 +0100

    [ADD] questions wip

--- src/app/src/game/QuestionController.js
+++ src/app/src/game/QuestionController.js
@@ -10,7 +10,37 @@ var question = angular.module('hiremeapp.question', [
     self.answers = null;
     self.explanation = null;
 
+    self.showNext = false;
+
+    $scope.counter = 1;
+    var mytimeout = 1;
+    var TIMER = 1000;
+
+    self.onTimeout = function(){
+      $scope.counter++;
+      mytimeout = $timeout(self.onTimeout,TIMER);
+    }
+
+    self.hitNext = function(){
+      $state.reload();
+    }
+
+    self.resetTimer = function(){
+      $timeout.cancel(mytimeout);
+      $scope.counter = 1;
+    }
+
     self.showQuestion = function(filter){
+      self.showNext = false;
+      mytimeout = $timeout(self.onTimeout,TIMER);
+      $scope.$watch('counter', function(nv) {
+        console.log(nv)
+          if(nv == 10){
+            self.resetTimer();
+            self.showNext = true;
+            $('md-card').addClass("active");
+          }
+      });
       questionServices.question('web','javascript',1).then(function successCallback(response) {
           self.question = response.data.question;
           self.answers = response.data.answers;
@@ -23,10 +53,10 @@ var question = angular.module('hiremeapp.question', [
 
     self.evaluateAnswer = function(answer, $event){
       if(answer.correct){
-        $($event.currentTarget).css("background-color", "green");
+        $($event.currentTarget).addClass("active");
         self.showSuccessDialog();
       } else{
-        $($event.currentTarget).css("background-color", "red");
+        $($event.currentTarget).addClass("active");
       }
     }
 
@@ -40,6 +70,7 @@ var question = angular.module('hiremeapp.question', [
             clickOutsideToClose:true
         })
             .then(function(answer) {
+            self.resetTimer();
             self.showQuestion(self.filters);
         }, function() {
             //canceled
